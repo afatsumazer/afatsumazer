8b15efd5baa0764945289ae0778d5328cd0da841
afatsumazer <273672+afatsumazer@users.noreply.github.com>
2026-07-21 06:06:06 +0700

    Add button for opening user profile modal

---
+++ js/@nama_pengguna.js
@@ -0,0 +1,4 @@
+<button onclick="openProfileModal('${file.senderUid}', '${file.senderUsername}')" 
+        class="text-blue-600 hover:text-blue-800 hover:underline font-medium focus:outline-none">
+    @${file.senderUsername || 'pengguna'}
+</button>
